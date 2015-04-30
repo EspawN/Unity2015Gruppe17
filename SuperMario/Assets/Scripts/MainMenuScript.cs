4eb83e070bd31e5ad67108cc739d016a79b5ad7f
Espen Kristiansen <espen.bebop@gmail.com>
2015-04-30 11:23:21 +0200

    Added Fonts, menu images and menu scripts

---
+++ SuperMario/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+	// Audio Variables
+	public AudioClip MainMenuMusic;
+
+
+	void Awake () {
+
+	}
+	
+	public void OnePlayer () {
+		Application.LoadLevel("Level1");
+	}
+		
+	public void HiScore () {
+		Application.LoadLevel("HighScores");
+	}
+
+	public void Back () {
+
+		Application.LoadLevel ("MainMenu");
+	}
+
+	public void Exit (){
+		Application.Quit ();
+	}
+}
